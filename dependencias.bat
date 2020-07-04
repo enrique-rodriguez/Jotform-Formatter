83517b97bea697e01baf358ab60482f744526c09
Kendrick Morales <kendrick.morales@upr.edu>
2020-07-04 17:59:06 -0400

    Nombre apropiado al file

---
+++ dependencias.bat
@@ -0,0 +1,15 @@
+pip install pdfplumber
+pip install altgraph
+pip install et_xmlfile
+pip install jdcal
+pip install macholib
+pip install numpy
+pip install openpyxl
+pip install pandas
+pip install PyInstaller
+pip install python-dateutil
+pip install pytz
+pip install six
+pip install xlrd
+
+pause
